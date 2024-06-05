f637413af7cdd4e677272e80a9f697634618da91
Eros Netto Antunes <erosnetto1002@gmail.com>
2024-06-04 22:12:28 -0300

    Teste

--- Programa-Medidas-Descritivas/vite.config.js
+++ Programa-Medidas-Descritivas/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Estatistica-ATV/Programa-Medidas-Descritivas",
+  base: "/Estatistica-ATV",
 });
