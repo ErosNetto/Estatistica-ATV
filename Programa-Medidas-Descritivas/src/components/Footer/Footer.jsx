486f211fc4e7ea52074fb7bb1a6d1f3ccf004f75
Eros Netto Antunes <erosnetto1002@gmail.com>
2024-06-04 22:02:14 -0300

    teste

--- Programa-Medidas-Descritivas/src/components/Footer/Footer.jsx
+++ Programa-Medidas-Descritivas/src/components/Footer/Footer.jsx
@@ -13,14 +13,14 @@ const Footer = () => {
         </div>
         <div className="footerDivItem">
           <div className="footerdDivColuna">
-            <h3>Eros Netto Antuens</h3>
-            <p>RA: 2023200550</p>
+            <h3>Gabriel Schultz do Amaral </h3>
+            <p>RA: 2024100666</p>
           </div>
         </div>
         <div className="footerDivItem">
           <div className="footerdDivColuna">
-            <h3>Eros Netto Antuens</h3>
-            <p>RA: 2023200550</p>
+            <h3>Vitor Hugo Kuerten Dechamps</h3>
+            <p>RA: 2024100726</p>
           </div>
         </div>
       </div>
